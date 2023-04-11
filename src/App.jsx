a3608193519fb45f4bb78b55399621338cf9b024
Nico <nicodiazan@gmail.com>
2023-04-11 09:44:49 +0200

    Remove some more unnecessary boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-
-    </div>
-  )
+	return <div className="App">Hello World</div>;
 }
 
-export default App
+export default App;
