904e89c746d1ad0b343abb0442323b6d04863f34
Nico <nicodiazan@gmail.com>
2023-04-11 16:18:39 +0200

    Add some basic handling to the search form

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,12 +4,23 @@ import { useGlobalContext } from "../Context";
 import { useState } from "react";
 
 const Search = () => {
+
+    const [text, setText] = useState("");
+
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
 	return (
         <header className="search-container">
-            <form >
-                <input type="text" className="form-input" />
+            <form onSubmit={onSubmit}>
+                <input type="text" className="form-input" value={text} placeholder="Spaghetti Carbonara" onChange={onChange} />
                 <button className="btn" type="submit">Search</button>
-                <button className="btn btn-hipster" type="btn">I'm feeling lucky</button>
+                <button className="btn btn-hipster" type="btn">I'm feeling lucky!</button>
             </form>
         </header>
     );
