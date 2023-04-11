e64564cc00ee201a91378adc387a769ed6f429ad
Nico <nicodiazan@gmail.com>
2023-04-11 13:08:36 +0200

    Add a loading state whilst fetching data

--- src/Context.jsx
+++ src/Context.jsx
@@ -10,24 +10,26 @@ const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 const AppProvider = ({ children }) => {
 	const [meals, setMeals] = useState([]);
-    const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
 
 	const fetchMeals = async (url) => {
-        setLoading(true);
+		setLoading(true);
 		try {
 			const res = await axios(url);
 			setMeals(res.data.meals);
 		} catch (err) {
 			console.error(err.response);
 		}
-        setLoading(false);
+		setLoading(false);
 	};
 
 	useEffect(() => {
 		fetchMeals(allMealsUrl);
 	}, []);
 	return (
-		<AppContext.Provider value={{ loading, meals }}>{children}</AppContext.Provider>
+		<AppContext.Provider value={{ loading, meals }}>
+			{children}
+		</AppContext.Provider>
 	);
 };
 
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -4,12 +4,15 @@ import { useContext } from "react";
 import { useGlobalContext } from "../Context";
 
 import Meal from "./Meal";
+import Loading from "./Loading";
 
 const Meals = () => {
 	const context = useGlobalContext();
 	return (
 		<section className="section-center">
-			{context.meals.map((meal) => (
+			{
+                context.loading ? <Loading /> :
+            context.meals.map((meal) => (
 				<Meal key={meal.idMeal} meal={meal} />
 			))}
 			<div>Forg kinda vibes rn</div>
