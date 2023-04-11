7880d2b6afea1415ec0f3a39efedafa75e575c0f
Nico <nicodiazan@gmail.com>
2023-04-11 13:26:53 +0200

    Add basic search header without functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { useGlobalContext } from "../Context";
+import { useState } from "react";
+
 const Search = () => {
-	return <div>Search</div>;
+	return (
+        <header className="search-container">
+            <form >
+                <input type="text" className="form-input" />
+                <button className="btn" type="submit">Search</button>
+                <button className="btn btn-hipster" type="btn">I'm feeling lucky</button>
+            </form>
+        </header>
+    );
 };
 
 export default Search;
