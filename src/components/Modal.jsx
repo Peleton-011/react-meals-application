d7b7b92eee565dc2d1a5f1db3d388011d73d4aee
Nico <nicodiazan@gmail.com>
2023-04-12 00:07:34 +0200

    Implement modals displaying full information on the selected meal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -13,7 +13,27 @@ const Modal = () => {
 
 	return (
 		<aside className="modal-overlay">
-			<div className="modal-container">:c No modal content...</div>
+			<div className="modal-container">
+				<img
+					src={image}
+					alt={`${title} recipe`}
+					className="img modal-img"
+				/>
+				<div className="modal-content">
+					<h4>{title}</h4>
+					<p>Cooking Instructions</p>
+					<p>{text}</p>
+					<a href={source} target="_blank">
+						Original Source
+					</a>
+					<button
+						className="btn btn-hipster close-btn"
+						onClick={closeModal}
+					>
+						close
+					</button>
+				</div>
+			</div>
 		</aside>
 	);
 };
