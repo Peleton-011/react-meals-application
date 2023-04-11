b4202d386e73bb5b4dc639e860ee8fcc776fc1f9
Nico <nicodiazan@gmail.com>
2023-04-11 23:34:25 +0200

    Add skeleton for the modal component

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 			<Search />
 			{/* <Favorites /> */}
 			<Meals />
-			{/* <Modal /> */}
+			<Modal />
 		</div>
 	);
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Modal = () => {
-	return <div>Modal</div>;
+	return (
+        <aside className="modal-overlay">
+            <div className="modal-container">
+                :c No modal content...
+            </div>
+        </aside>
+    );
 };
 
 export default Modal;
