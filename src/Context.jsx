c2fcd0f4eaaa70e5108883932149e3ddc23ab484
Nico <nicodiazan@gmail.com>
2023-04-11 10:40:07 +0200

    Add a sample useEffect for future use

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+
+    useEffect(() => {
+        console.log("Test useEffect")
+    }, []);
 	return <AppContext.Provider value="test-1">{children}</AppContext.Provider>;
 };
 
