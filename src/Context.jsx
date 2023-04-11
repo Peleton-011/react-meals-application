bb0a809c1246b90425e664df729d2996c831ce96
Nico <nicodiazan@gmail.com>
2023-04-11 10:55:57 +0200

    Minor refactor for readability

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,20 +3,20 @@ import { useContext, useEffect } from "react";
 
 const AppContext = React.createContext();
 
+const fetchData = async () => {
+	try {
+		const res = await fetch("https://randomuser.me/api/");
+		const data = await res.json();
+		console.log(data);
+	} catch (err) {
+		console.error(err);
+	}
+};
+
 const AppProvider = ({ children }) => {
 	useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch("https://randomuser.me/api/")
-                const data = await res.json();
-                console.log(data);
-            } catch (err) {
-                console.error(err);
-            }
-        }
-
-        fetchData();
-    }, []);
+		fetchData();
+	}, []);
 	return <AppContext.Provider value="test-1">{children}</AppContext.Provider>;
 };
 
