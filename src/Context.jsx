e05f4e04dcb44eed5563cadd5484e57128f0c87c
Nico <nicodiazan@gmail.com>
2023-04-11 11:03:29 +0200

    Add URLs from the desired meal API

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,6 +3,9 @@ import { useContext, useEffect } from "react";
 
 const AppContext = React.createContext();
 
+const allMealsUrl = "www.themealdb.com/api/json/v1/1/search.php?s=";
+const randomMealUrl = "www.themealdb.com/api/json/v1/1/random.php";
+
 const fetchData = async () => {
 	try {
 		const res = await fetch("https://randomuser.me/api/");
