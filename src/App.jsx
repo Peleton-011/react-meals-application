1e57072e651680ebd8a6896d07bb5d2401e148bf
Nico <nicodiazan@gmail.com>
2023-04-11 09:52:21 +0200

    Add skeleton for some future components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 
+import Favorites from "./components/Favorites";
+import Meals from "./components/Meals";
+import Modal from "./components/Modal";
+import Search from "./components/Search";
+
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <div className="App">Hello World</div>;
+	return (
+		<div className="App">
+			<Favorites />
+			<Meals />
+			<Modal />
+			<Search />
+		</div>
+	);
 }
 
 export default App;
