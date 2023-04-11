3c1c8c2054f2c2c4702fb154e40179b94be0b70e
Nico <nicodiazan@gmail.com>
2023-04-11 12:34:31 +0200

    Add like icon from react-icons

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
+import { BsHandThumbsUp } from "react-icons/bs";
+
 const Meal = ({ meal }) => {
 	const { idMeal, strMeal: title, strMealThumb: image } = meal;
 	return (
 		<article key={idMeal} className="single-meal">
-			<img
-				src={image}
-				alt={`${title} thumbnail`}
-				className="img"
-			/>
+			<img src={image} alt={`${title} thumbnail`} className="img" />
 			<footer>
 				<h5>{title}</h5>
-				<button className="like-btn">click me!</button>
+				<button className="like-btn">
+					<BsHandThumbsUp />
+				</button>
 			</footer>
 		</article>
 	);
