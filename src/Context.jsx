061e854f695d11c4066d8053815153130a128016
Nico <nicodiazan@gmail.com>
2023-04-11 10:54:38 +0200

    Set up some sample data fetching

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,14 +4,22 @@ import { useContext, useEffect } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+	useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("https://randomuser.me/api/")
+                const data = await res.json();
+                console.log(data);
+            } catch (err) {
+                console.error(err);
+            }
+        }
 
-    useEffect(() => {
-        console.log("Test useEffect")
+        fetchData();
     }, []);
 	return <AppContext.Provider value="test-1">{children}</AppContext.Provider>;
 };
 
-const useGlobalContext = () => useContext(AppContext)
+const useGlobalContext = () => useContext(AppContext);
 
-export { AppContext, AppProvider, useGlobalContext};
-
+export { AppContext, AppProvider, useGlobalContext };
