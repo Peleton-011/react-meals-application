ab943a3ad501a7a2f0942e3d3c7e6763e0e0eccd
Nico <nicodiazan@gmail.com>
2023-04-13 22:04:31 +0200

    Implement a remove favorite button

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -3,28 +3,41 @@ import React from "react";
 import { useGlobalContext } from "../Context";
 
 const Favorites = () => {
-    const { favorites, selectMeal, removeFromFavorites } = useGlobalContext();
+	const { favorites, selectMeal, removeFromFavorites } = useGlobalContext();
 	return (
-        <section className="favorites">
-            <div className="favorites-content">
-                <h5>Favorites</h5>
-                <div className="favorites-container">
-                    {
-                        favorites.map((favorite) => {
-                            const {idMeal, strMealThumb: image, strMeal: title} = favorite;
+		<section className="favorites">
+			<div className="favorites-content">
+				<h5>Favorites</h5>
+				<div className="favorites-container">
+					{favorites.map((favorite) => {
+						const {
+							idMeal,
+							strMealThumb: image,
+							strMeal: title,
+						} = favorite;
 
-                            return (
-                                <div key={idMeal} className="favorite-item">
-                                    <img src={image} alt={`${title}`} className="favorites-img img" />
-                                    <button className="remove-btn"></button>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
-        </section>
-    );
+						return (
+							<div key={idMeal} className="favorite-item">
+								<img
+									src={image}
+									alt={`${title}`}
+									className="favorites-img img"
+								/>
+								<button
+									className="remove-btn"
+									onClick={() => {
+										removeFromFavorites(idMeal);
+									}}
+								>
+									Remove
+								</button>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Favorites;
